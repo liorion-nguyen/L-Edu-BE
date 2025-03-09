4e1778ee8b2d7de9d5e7d277c53fff60cb2f0ea3
LiorionNguyen <liorion.nguyen@gmail.com>
2025-03-09 21:57:38 +0700

    fix: deployment

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as basicAuth from "express-basic-auth";
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+const basicAuth = require("express-basic-auth");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
