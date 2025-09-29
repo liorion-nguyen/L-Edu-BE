c51a28547f2d4b8caf5320c14fe76664bf4d4617
Liorion Nguyen <liorion.nguyen@gmail.com>
2025-09-29 17:55:59 +0700

    fix: build

--- src/api/auth/dto/change-password.dto.ts
+++ src/api/auth/dto/change-password.dto.ts
@@ -9,3 +9,4 @@ export class ChangePasswordDto {
   @MinLength(6)
   newPassword: string;
 }
+
--- src/scheme/email-verification.schema.ts
+++ src/scheme/email-verification.schema.ts
@@ -19,3 +19,4 @@ export class EmailVerification {
 }
 
 export const EmailVerificationSchema = SchemaFactory.createForClass(EmailVerification);
+
